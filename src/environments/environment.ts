c57fdf0918b7ffb385a21922344742aa1b19c67e
gsuvo <joguaguilarco@ittepic.edu.mx>
2025-07-22 16:27:43 -0700

    Vercel fix apiurl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'https://y-tfav-backend.vercel.app',
+  apiUrl: 'https://y-tfav-backend.vercel.app/api',
   YOUTUBE_API_KEY: '',
   RECAPTCHA_SITE_KEY: ''
 };
