a4964e26dd95d2645ab89d888b7c0b5099f9a05c
gsuvo <joguaguilarco@ittepic.edu.mx>
2025-07-22 15:24:47 -0700

    fix env imports

---
+++ scripts/generate-env.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+const envProdPath = path.join(__dirname, '../src/environments/environment.prod.ts');
+
+const content = `export const environment = {
+  production: true,
+  apiUrl: '${process.env.API_URL || ''}',
+  YOUTUBE_API_KEY: '${process.env.YOUTUBE_API_KEY || ''}',
+  RECAPTCHA_SITE_KEY: '${process.env.RECAPTCHA_SITE_KEY || ''}'
+};
+`;
+
+fs.writeFileSync(envProdPath, content);
+console.log('✔️  environment.prod.ts generado correctamente');
